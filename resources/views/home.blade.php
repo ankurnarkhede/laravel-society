e986f5ba49e42b464d7ac37148840afb2974be00
smartankur4u <ankurnarkhede@gmail.com>
2018-04-11 00:59:26 +0530

    added materialize

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,217 +1,203 @@
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="{{ LAConfigs::getByKey('site_description') }}">
-    <meta name="author" content="Dwij IT Solutions">
-
-    <meta property="og:title" content="{{ LAConfigs::getByKey('sitename') }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="{{ LAConfigs::getByKey('site_description') }}" />
-    
-    <meta property="og:url" content="http://laraadmin.com/" />
-    <meta property="og:sitename" content="laraAdmin" />
-	<meta property="og:image" content="http://demo.adminlte.acacha.org/img/LaraAdmin-600x600.jpg" />
-    
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:site" content="@laraadmin" />
-    <meta name="twitter:creator" content="@laraadmin" />
-    
-    <title>{{ LAConfigs::getByKey('sitename') }}</title>
-    
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('/la-assets/css/bootstrap.css') }}" rel="stylesheet">
-
-	<link href="{{ asset('la-assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
-    
-    <!-- Custom styles for this template -->
-    <link href="{{ asset('/la-assets/css/main.css') }}" rel="stylesheet">
-
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
-
-    <script src="{{ asset('/la-assets/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-    <script src="{{ asset('/la-assets/js/smoothscroll.js') }}"></script>
 
 
+
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" name="viewport">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="no" name="msapplication-tap-highlight">
+    <meta content="OnlineSGGS Invigilation Portal " name="description">
+    <meta content="SGGS, SGGSIE&T, Invigilation, notifications, invigilation scheduling, schedule, OnlineSGGS, onlinesggs, sggs, invigilation, INVIGILATION, portal, PORTAL, Portal, invigilation portalShri Guru Gobind Singhji Institute of Engineering and Technology, nanded, vishnupuri," name="keywords">
+    <meta name="author" content="Ankur Narkhede" />
+
+
+    <title>Login | SGGSIE&T Invigilation</title>
+    <!-- Favicons-->
+    <link rel="icon" type="image/png" href="assets/images/sggs_logo.png" sizes="32x32" />
+
+    <meta content="#00bcd4" name="msapplication-TileColor">
+    <meta content="login_page/images/favicon/mstile-144x144.png" name="msapplication-TileImage"><!-- For Windows Phone -->
+    <!-- CORE CSS-->
+    <link href="login_page/css/materialize.min.css" media="screen,projection" rel="stylesheet" type="text/css">
+    <link href="login_page/css/style.min.css" media="screen,projection" rel="stylesheet" type="text/css"><!-- Custome CSS-->
+    <link href="login_page/css/custom/custom.min.css" media="screen,projection" rel="stylesheet" type="text/css">
+    <link href="login_page/css/layouts/page-center.css" media="screen,projection" rel="stylesheet" type="text/css"><!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
+    <link href="login_page/js/plugins/prism/prism.css" media="screen,projection" rel="stylesheet" type="text/css">
+    <link href="login_page/js/plugins/perfect-scrollbar/perfect-scrollbar.css" media="screen,projection" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="assets/css/style.css?no-cache=<?php echo time(); ?>" rel="stylesheet" type="text/css">
+
+
+    {{--analytics--}}
+    <?php
+
+    include "assets/analytics.php";
+
+    ?>
+
+    <script>
+
+        $(document).ready(function(){
+            $("#login_btn").click(function(){
+                $("#signup").fadeOut();
+                $("#login").fadeIn();
+
+            });
+        });
+
+        $(document).ready(function(){
+            $("#signup_btn").click(function(){
+                $("#login").fadeOut();
+                $("#signup").fadeIn();
+
+            });
+        });
+
+
+    </script>
+
+
+    <style>
+
+    </style>
 </head>
 
-<body data-spy="scroll" data-offset="0" data-target="#navigation">
+<body class="cyan">
+<!-- Start Page Loading -->
 
-<!-- Fixed navbar -->
-<div id="navigation" class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"><b>{{ LAConfigs::getByKey('sitename') }}</b></a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#home" class="smoothScroll">Home</a></li>
-                <li><a href="#about" class="smoothScroll">About</a></li>
-                <li><a href="#contact" class="smoothScroll">Contact</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <!--<li><a href="{{ url('/register') }}">Register</a></li>-->
-                @else
-                    <li><a href="{{ url(config('laraadmin.adminRoute')) }}">{{ Auth::user()->name }}</a></li>
-                @endif
-            </ul>
-        </div><!--/.nav-collapse -->
+
+<div id="loader-wrapper">
+    <div id="loader">
+    </div>
+
+
+    <div class="loader-section section-left">
+    </div>
+
+
+    <div class="loader-section section-right">
     </div>
 </div>
+<!-- End Page Loading -->
 
 
-<section id="home" name="home"></section>
-<div id="headerwrap">
-    <div class="container">
-        <div class="row centered">
-            <div class="col-lg-12">
-                <h1>{{ LAConfigs::getByKey('sitename_part1') }} <b><a>{{ LAConfigs::getByKey('sitename_part2') }}</a></b></h1>
-                <h3>{{ LAConfigs::getByKey('site_description') }}</h3>
-                <h3><a href="{{ url('/login') }}" class="btn btn-lg btn-success">Get Started!</a></h3><br>
-            </div>
-            <div class="col-lg-2">
-                <h5>Amazing Functionalities</h5>
-                <p>for Modern Admin Panels</p>
-                <img class="hidden-xs hidden-sm hidden-md" src="{{ asset('/la-assets/img/arrow1.png') }}">
-            </div>
-            <div class="col-lg-8">
-                <img class="img-responsive" src="{{ asset('/la-assets/img/app-bg.png') }}" alt="">
-            </div>
-            <div class="col-lg-2">
-                <br>
-                <img class="hidden-xs hidden-sm hidden-md" src="{{ asset('/la-assets/img/arrow2.png') }}">
-                <h5>Completely Packaged...</h5>
-                <p>for Future expantion of Modules</p>
-            </div>
-        </div>
-    </div> <!--/ .container -->
-</div><!--/ #headerwrap -->
+<div class="row" id="login-page">
+
+    <div class="col s12 z-depth-5 card-panel">
+        <form action=" {{ url('/login') }}" class="login-form" enctype="multipart/form-data" method="post" style="width: 400px;">
+            <div class="row" style="margin-bottom: 0px;">
+                <div class="input-field col s12 center">
+                    <img alt="" class="circle responsive-img valign profile-image-login" src="assets/images/home-logo.png" style="width: unset;">
+
+                    <p class="center login-form-text">SOCIETY</p>
 
 
-<section id="about" name="about"></section>
-<!-- INTRO WRAP -->
-<div id="intro">
-    <div class="container">
-        <div class="row centered">
-            <h1>An Architecture designed To Excel</h1>
-            <br>
-            <br>
-            <div class="col-lg-4">
-                <i class="fa fa-cubes" style="font-size:100px;height:110px;"></i>
-                <h3>Modular</h3>
-                <p>Making Data Management fast and enjoyable.</p>
-            </div>
-            <div class="col-lg-4">
-                <i class="fa fa-paper-plane" style="font-size:100px;height:110px;"></i>
-                <h3>Easy to Install</h3>
-                <p>With single installation command.</p>
-            </div>
-            <div class="col-lg-4">
-                <i class="fa fa-cubes" style="font-size:100px;height:110px;"></i>
-                <h3>Customizable</h3>
-                <p>Easy to Manipulation the flows.</p>
-            </div>
-        </div>
-        <br>
-        <hr>
-    </div> <!--/ .container -->
-</div><!--/ #introwrap -->
-
-<!-- FEATURES WRAP -->
-<div id="features">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-5 centered">
-                <img class="centered" src="{{ asset('/la-assets/img/mobile.png') }}" alt="">
-            </div>
-
-            <div class="col-lg-7">
-				<h3 class="feature-title">What is LaraAdmin ?</h3><br>
-				<ol class="features">
-					<li><strong>CMS</strong> (Content Management System) &#8211; Manages Modules &amp; their Data</li>
-					<li>Backend <strong>Admin Panel</strong> &#8211; Data can be used in front end applications with ease.</li>
-					<li>A probable <strong>CRM</strong> System &#8211; Can be evolved into a CRM system like <a target="_blank" href="https://www.sugarcrm.com">SugarCRM</a></li>
-				</ol><br>
-
-				<h3 class="feature-title">Why LaraAdmin ?</h3><br>
-                <ol class="features">
-					<li><strong>Philosophy:</strong> Inspired by SugarCRM &amp; based on Advanced <strong>Data Types</strong> like Image, HTML, File, Dropdown, TagInput which makes developers job easy. See more in <a target="_blank" href="http://laraadmin.com/features">features</a></li>
-					<li>Superior <strong>CRUD generation</strong> for Modules which generates Migration, Controller, Model and Views with single artisan command and integrates with Routes as as well.</li>
-					<li><strong>Form Maker</strong> helper is provided for generating entire form with single function call with module name as single parameter. It also gives you freedom to customise form for every field by providing method to generate single field with parameters for customisations.</li>
-					<li><b>Upload Manager </b>manages project files &amp; images which are integrated with your Module fields.</li>
-					<li><strong>Menu Manager</strong> creates menu with Modules &amp; Custom Links likes WordPress</li>
-					<li><strong>Online Code Editor</strong> allows developers to customise the generated Module Views &amp; Files.</li>
-				</ol>
-            </div>
-        </div>
-    </div><!--/ .container -->
-</div><!--/ #features -->
-
-<section id="contact" name="contact"></section>
-<div id="footerwrap">
-    <div class="container">
-        <div class="col-lg-5">
-            <h3>Contact Us</h3><br>
-            <p>
-				Dwij IT Solutions,<br/>
-				Web Development Company in Pune,<br/>
-                B4, Patang Plaza Phase 5,<br/>
-                Opp. PICT College,<br/>
-                Katraj, Pune, India - 411046
-            </p>
-			<div class="contact-link"><i class="fa fa-envelope-o"></i> <a href="mailto:hello@laraadmin.com">hello@laraadmin.com</a></div>
-			<div class="contact-link"><i class="fa fa-cube"></i> <a href="http://laraadmin.com">laraadmin.com</a></div>
-			<div class="contact-link"><i class="fa fa-building"></i> <a href="http://dwijitsolutions.com">dwijitsolutions.com</a></div>
-        </div>
-
-        <div class="col-lg-7">
-            <h3>Drop Us A Line</h3>
-            <br>
-            <form role="form" action="#" method="post" enctype="plain">
-                <div class="form-group">
-                    <label for="name1">Your Name</label>
-                    <input type="name" name="Name" class="form-control" id="name1" placeholder="Your Name">
+                    <p class="center" style="color: #e74c3c; font-weight: bold;">
+                    </p>
                 </div>
-                <div class="form-group">
-                    <label for="email1">Email address</label>
-                    <input type="email" name="Mail" class="form-control" id="email1" placeholder="Enter email">
+            </div>
+
+
+            <div class="row">
+                <div class="col s12">
+                    <ul class="tabs">
+                        <li class="tab col s6">
+                            <a class="active" href="#login" id="login_btn">ADMIN LOGIN</a>
+                        </li>
+
+                    </ul>
+
+                    {{--if error--}}
+
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                 </div>
-                <div class="form-group">
-                    <label>Your Text</label>
-                    <textarea class="form-control" name="Message" rows="3"></textarea>
+
+                <div class="col s12">
+                    {{--@if(Session::has('result'))--}}
+                    {{--<div>--}}
+                    {{--<p class="text_center red-text text-bold">--}}
+                    {{--{{ Session::get('result') }}--}}
+                    {{--</p>--}}
+                    {{--</div>--}}
+                    {{--@endif--}}
+
+                    {{--@if(count($errors)>0)--}}
+                    {{--<div>--}}
+                    {{--<ul>--}}
+                    {{--@foreach($errors->all() as $error)--}}
+                    {{--<li>{{ $error }}</li>--}}
+                    {{--@endforeach--}}
+                    {{--</ul>--}}
+                    {{--</div>--}}
+                    {{--@endif--}}
                 </div>
-                <br>
-                <button type="submit" class="btn btn-large btn-success">SUBMIT</button>
-            </form>
-        </div>
+
+
+                <div class="col s12" id="login">
+                    <div class="row margin">
+                        <div class="input-field col s12">
+                            <i class="mdi-social-person-outline prefix"></i> <input id="email" name="email" type="text"> <label class="center-align" for="email">Email or Phone</label>
+                        </div>
+                    </div>
+
+
+                    <div class="row margin">
+                        <div class="input-field col s12">
+                            <i class="mdi-action-lock-outline prefix"></i> <input id="password" name="password" type="password"> <label for="password">Password</label>
+                        </div>
+                    </div>
+
+
+                    <input type="hidden" name="_token" value="{{ Session::token() }}">
+
+
+                    <div class="row">
+                        <div class="input-field col s12" type="submit">
+                            <button class="btn waves-effect waves-light col s12" name="login_submit" type="submit">LogIn</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+
+        <p class="text_center">Developed by <a href="https://www.linkedin.com/in/ankurnarkhede">Ankur Narkhede</a></p>
+
     </div>
 </div>
-<div id="c">
-    <div class="container">
-        <p>
-            <strong>Copyright &copy; 2016. Powered by <a href="https://dwijitsolutions.com"><b>Dwij IT Solutions</b></a>
-        </p>
-    </div>
-</div>
+<!-- ================================================
+Scripts
+================================================ -->
+<!-- jQuery Library -->
+<script src="login_page/js/plugins/jquery-1.11.2.min.js" type="text/javascript">
+</script> <!--materialize js-->
 
+<script src="login_page/js/materialize.min.js" type="text/javascript">
+</script> <!--prism-->
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="{{ asset('/la-assets/js/bootstrap.min.js') }}" type="text/javascript"></script>
-<script>
-    $('.carousel').carousel({
-        interval: 3500
-    })
+<script src="login_page/js/plugins/prism/prism.js" type="text/javascript">
+</script> <!--scrollbar-->
+
+<script src="login_page/js/plugins/perfect-scrollbar/perfect-scrollbar.min.js" type="text/javascript">
+</script> <!--plugins.js - Some Specific JS codes for Plugin Settings-->
+
+<script src="login_page/js/plugins.min.js" type="text/javascript">
+</script> <!--custom-script.js - Add your own theme custom JS-->
+
+<script src="login_page/js/custom-script.js" type="text/javascript">
 </script>
 </body>
 </html>
